bb0e489268be4108fa4625158ade07b2762e98ff
kevincalixte <kevin.calixte@icloud.com>
2025-12-20 01:10:15 +0100

    type: modified

--- components/Content/Reviews.tsx
+++ components/Content/Reviews.tsx
@@ -2,29 +2,37 @@ import { MdQuestionAnswer } from "react-icons/md";
 import React, { useEffect, useState } from 'react'
 import { FiHelpCircle } from "react-icons/fi";
 
+type Review = {
+    id: string;
+    name: string;
+    subject: string;
+    message: string;
+}
+
 export const Reviews = () => {
 
-    const [reviews, setReviews] = useState([]);
+    const [reviews, setReviews] = useState<Review[]>([]);
+    const [title, setTitle] = useState<string>('Ask Us Anything');
 
     useEffect(() => {
         fetch('/data/reviews.json')
             .then(response => response.json())
             .then(data => {
-                console.log('>>>', data);
                 setReviews(data);
             })
+            .catch(err => setTitle("Reviews are unavailable"));
     }, []);
 
 
     return (
         <>
-            <h2 className='mb-10 flex justify-center items-center gap-2'><FiHelpCircle />Ask Us Anything</h2>
+            <h2 className='mb-10 flex justify-center items-center gap-2'><FiHelpCircle />{title}</h2>
             <section className='flex overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-hide'>
                 {
-                    reviews.map((review: any) => (
+                    reviews.map((review) => (
 
                         <div className='flex justify-center shrink-0 max-w-[80%] px-2 snap-start'>
-                            <article id={review.id} className='flex flex-col items-center gap-1 p-5 rounded-xs bg-gray-300 text-black hover:bg-white hover:cursor-pointer '>
+                            <article key={review.id} className='flex flex-col items-center gap-1 p-5 rounded-xs bg-gray-300 text-black hover:bg-white hover:cursor-pointer '>
                                 <h3>{review.name}</h3>
                                 <p className='text-xs text-black-300'>About : {review.subject}</p>
                                 <p>{review.message}</p>
