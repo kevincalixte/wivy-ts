2ab516307c924448b54d2a7c42dd0744afcc4475
kevincalixte <kevin.calixte@icloud.com>
2025-12-22 22:07:47 +0100

    signup: fixing

--- components/Signup/Signup.tsx
+++ components/Signup/Signup.tsx
@@ -1,23 +1,37 @@
+import { useEffect } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import Button from "../Button/Button"
-import Link from "next/link";
 import { Border } from "../Content/Border";
+import { useState } from "react";
 
 type SignupProps = {
     onClose: () => void;
 }
 
 export const Signup = ({ onClose }: SignupProps) => {
+
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, []);
+
+    const [isSignin, setIsSignin] = useState(false);
+
     return (
         <>
             <div className='flex flex-col items-center justify-center w-screen h-screen bg-black/60 backdrop-blur-xl fixed'>
                 <div>
-                    <Link href={''}> <AiOutlineClose className="absolute top-0 right-0 m-3 text-2xl" /></Link>
-                    <h2 className="text-3xl mb-10">Sign Up</h2>
-                    <form method='' className='space-y-4 [&_input]:text-lg [&_input]:w-[70%] [&_input]:py-2 [&_input]:border-b-2 [&_input]:border-b-gray-400 [&_input]:focus:outline-0'>
-                        <input type='text' name='name' placeholder='Your name' />
-                        <input type='text' name='name' placeholder='Your Birth Day' />
+                    <AiOutlineClose onClick={onClose} className="absolute top-0 right-0 m-3 text-2xl" />
+                    <h2 className="text-3xl mb-10">{isSignin ? 'Sign In' : 'Sign Up'}</h2>
+                    <form method='' className='space-y-4 [&_input]:text-lg [&_input]:w-[70vw] [&_input]:py-2 [&_input]:border-b-2 [&_input]:border-b-gray-400 [&_input]:focus:outline-0'>
+                        <input className={`${isSignin ? 'hidden' : ''}`} type='text' name='name' placeholder='Your name' />
+                        <input className={`${isSignin ? 'hidden' : ''}`} type='text' name='name' placeholder='Your Birth Day' />
                         <input type='number' name='email' placeholder='Your Phone Number' />
+                        {isSignin ? <>
+                            <input type='password' name='password' placeholder='Your Password' />
+                        </> : ''}
                         <Button className='px-5 mt-3 rounded-xs text-gray-300 block mx-auto border'>Validate</Button>
                     </form>
                 </div>
@@ -26,7 +40,7 @@ export const Signup = ({ onClose }: SignupProps) => {
 
                 <div>
                     <h2 className="text-xl mb-5 -mt-5">Or</h2>
-                    <Button className="px-5 rounded-xs text-gray-300 border">Sign In</Button>
+                    <Button onClick={() => setIsSignin(!isSignin)} className="px-5 rounded-xs text-gray-300 border">{isSignin ? 'Sign Up' : 'Sign In'}</Button>
                 </div>
 
             </div>
