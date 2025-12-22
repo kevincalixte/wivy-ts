b052fe37f27f1e18cd516cf7b4f05ea2998cb89b
kevincalixte <kevin.calixte@icloud.com>
2025-12-22 21:31:59 +0100

    signup: fixing

--- components/Signup/Signup.tsx
+++ components/Signup/Signup.tsx
@@ -13,12 +13,12 @@ export const Signup = ({ onClose }: SignupProps) => {
             <div className='flex flex-col items-center justify-center w-screen h-screen bg-black/60 backdrop-blur-xl fixed'>
                 <div>
                     <Link href={''}> <AiOutlineClose className="absolute top-0 right-0 m-3 text-2xl" /></Link>
-                    <h2 className="text-2xl mb-5">Sign Up</h2>
-                    <form method='' className='space-y-4 [&_input]:w-50 [&_input]:py-2 [&_input]:border-b-2 [&_input]:border-b-gray-400 [&_input]:focus:outline-0'>
+                    <h2 className="text-3xl mb-10">Sign Up</h2>
+                    <form method='' className='space-y-4 [&_input]:text-lg [&_input]:w-[70%] [&_input]:py-2 [&_input]:border-b-2 [&_input]:border-b-gray-400 [&_input]:focus:outline-0'>
                         <input type='text' name='name' placeholder='Your name' />
                         <input type='text' name='name' placeholder='Your Birth Day' />
                         <input type='number' name='email' placeholder='Your Phone Number' />
-                        <Button className='px-5 mt-3 text-sm rounded-xs text-gray-300 block mx-auto border'>Validate</Button>
+                        <Button className='px-5 mt-3 rounded-xs text-gray-300 block mx-auto border'>Validate</Button>
                     </form>
                 </div>
 
@@ -26,7 +26,7 @@ export const Signup = ({ onClose }: SignupProps) => {
 
                 <div>
                     <h2 className="text-xl mb-5 -mt-5">Or</h2>
-                    <Button className="px-5 text-sm rounded-xs text-gray-300 border">Sign In</Button>
+                    <Button className="px-5 rounded-xs text-gray-300 border">Sign In</Button>
                 </div>
 
             </div>
